5bacda06a082c37af19911e1ab61497ec205ee79
RohitSatheeshNair <71492948+RohitSatheeshNair@users.noreply.github.com>
2021-12-03 00:03:47 +0530

    Update Lab1_tictactoe.py

--- Lab1_tictactoe.py
+++ Lab1_tictactoe.py
@@ -1,99 +1,123 @@
-import time
+import random
 
-# Initialize board
-board = { 1 : ' ', 2 : ' ', 3: ' ',
-         4 : ' ', 5 : ' ', 6 : ' ', 
-         7 : ' ', 8 : ' ', 9 : ' '}
 
-# Initialize variables
-count = 0		# counter to track number of filled slots
-winner = False		# boolean to check if anyone won
-play = True		# boolen to check if the game should continue
-tie = False		# boolean to check if there is a tie
-curr_player = ''	# variable to store current player identifier
-player_details = []	# list to store player identifier and marker
+class TicTacToe:
 
-# Helper functions
-def get_player_details(curr_player):
-    """Function to get player identifier and marker"""
-    if curr_player == 'A':
-        return ['B','O']
-    else:
-        return ['A','X']
-    
+    def __init__(self):
+        self.board = []
 
-def print_board():
-    """Function to print the board"""
-    for i in board:
-        print( i, ':', board[i], ' ', end='')
-        if i%3 == 0:
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            self.board.append(row)
+
+    def get_random_first_player(self):
+        return random.randint(0, 1)
+
+    def fix_spot(self, row, col, player):
+        self.board[row][col] = player
+
+    def is_player_win(self, player):
+        win = None
+
+        n = len(self.board)
+
+        
+        for i in range(n):
+            win = True
+            for j in range(n):
+                if self.board[i][j] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        
+        for i in range(n):
+            win = True
+            for j in range(n):
+                if self.board[j][i] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        
+        win = True
+        for i in range(n):
+            if self.board[i][i] != player:
+                win = False
+                break
+        if win:
+            return win
+
+        win = True
+        for i in range(n):
+            if self.board[i][n - 1 - i] != player:
+                win = False
+                break
+        if win:
+            return win
+        return False
+
+        for row in self.board:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
+
+    def is_board_filled(self):
+        for row in self.board:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
+
+    def swap_player_turn(self, player):
+        return 'X' if player == 'O' else 'O'
+
+    def show_board(self):
+        for row in self.board:
+            for item in row:
+                print(item, end=" ")
             print()
 
+    def start(self):
+        self.create_board()
 
-def win_game(marker, player_id):
-    """Function to check for winning combination"""
-    if board[1] == marker and board[2] == marker and board[3] == marker or \
-    board[4] == marker and board[5] == marker and board[6] == marker or \
-    board[7] == marker and board[8] == marker and board[9] == marker or \
-    board[1] == marker and board[4] == marker and board[7] == marker or \
-    board[2] == marker and board[5] == marker and board[8] == marker or \
-    board[3] == marker and board[6] == marker and board[9] == marker or \
-    board[1] == marker and board[5] == marker and board[9] == marker or \
-    board[3] == marker and board[5] == marker and board[7] == marker:
+        player = 'X' if self.get_random_first_player() == 1 else 'O'
+        while True:
+            print(f"Player {player} turn")
 
-        print_board()
-        time.sleep(1)
-        print("Player", player_id, "wins!")
-        return True
+            self.show_board()
 
-    else:
-        return False
+            row, col = list(
+                map(int, input("Enter row and column numbers to fix spot: ").split()))
+            print()
+
+           
+            self.fix_spot(row - 1, col - 1, player)
+
+            
+            if self.is_player_win(player):
+                print(f"Player {player} wins the game!")
+                break
+
+           
+            if self.is_board_filled():
+                print("Match Draw!")
+                break
+
+            
+            player = self.swap_player_turn(player)
+
+        
+        print()
+        self.show_board()
 
 
-def insert_input(slot_num, marker):
-    """Function for capturing user inputs"""
-    while board[slot_num] != ' ':
-        print("spot taken, pick another no.")
-        slot_num = int(input())
-    board[slot_num] = marker
 
-def play_again():
-    """Function to check if player wants to play again"""
-    print("Do you want to play again?")
-    play_again = input()
-
-    if play_again.upper() == 'Y':
-        for z in board:
-            board[z] = ' '
-        return True
-    else:
-        print("Thanks for playing. See you next time!")
-        return False
-    
-# Main program
-while play:
-
-    print_board()
-
-    player_details = get_player_details(curr_player)
-    curr_player = player_details[0]
-    print("Player {}: Enter a number between 1 and 9".format(curr_player))
-    input_slot = int(input())
-
-    #Inserting 'X' or 'O' in desired spot
-    insert_input(input_slot, player_details[1])
-    count += 1
-    
-    # Check if anybody won
-    winner = win_game(player_details[1], curr_player)
-    if count == 9 and not winner:
-        print("It's a tie!!")
-        tie = True
-        print_board()
-
-    # Check if players want to play again
-    if winner or tie:
-        play = play_again()
-        if play:
-            curr_player = ''
-            count = 0
+tic_tac_toe = TicTacToe()
+tic_tac_toe.start()
